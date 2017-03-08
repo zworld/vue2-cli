b57590eb1d520f6cf570e3319a245e7d312f4000
zhongxinzhi <1123858432@qq.com>
2017-03-08 19:16:29 +0800

    搭建框架,配置babel

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require("path");
+const webpack = require("webpack");
+
+let config = {
+    entry: {
+        app: path.join(__dirname,"./src/test/app.es6"),
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        // publicPath: "/web-cli-websec/web/app/",
+        filename: "js/[name].js",
+        // chunkFilename: "lib/[id].bundle.js"
+    },
+    //加载器
+    module: {
+        // 加载器
+        loaders: [
+            // 解析.vue文件
+            // {
+            //     test: /\.vue$/,
+            //     loader: "vue"
+            // },
+            // 转化ES6的语法
+            {
+                test: /\.(js|es6)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    "presets": ["babel-preset-es2015","babel-preset-stage-2"],
+                    "plugins": ["transform-runtime"]
+
+                }
+            }
+            // 编译css并自动添加css前缀
+            // {
+            //     test: /\.css$/,
+            //     loader: CSSExtractPlugin.extract("style-loader", "css!autoprefixer")
+            // },
+            //.less 文件想要编译，scss就需要这些东西！来编译处理
+            //install css-loader style-loader less-loader --save-dev
+            // {
+            //     test: /\.less$/,
+            //     loader: CSSExtractPlugin.extract("style-loader", "css!autoprefixer!less")
+            // },
+            // 图片转化，小于8K自动转化为base64的编码
+            // {
+            //     test: /\.(png|jpg|gif|svg)$/,
+            //     loader: "url-loader",
+            //     query: {
+            //         limit: 8192,
+            //         name: "/images/[name].[ext]"
+            //     }
+            //     //在这无论是直接loader 后面跟参数(像url跟参一样)url-loader?limit=8192,或者是后面跟着一个对象 query,都是可以的.类似get请求？
+            // },
+            //字体？
+            // {
+            //     test: /\.((eot|ttf|otf|woff|woff2)(\?.*)?|svg(\?.*))$/,
+            //     loader: "url-loader",
+            //     query: {
+            //         limit: 1,
+            //         name: "/fonts/[name].[ext]"
+            //     }
+            // },
+            //html模板编译？
+            // {
+            //     test: /\.(html|tpl)$/,
+            //     loader: "html-loader"
+            // }
+        ]
+    },
+    // .vue的配置。需要单独出来配置
+    // vue: {
+    //     loaders: {
+    //         css: VueCSSExtractPlugin.extract("style-loader", "css!autoprefixer"),
+    //         less: VueCSSExtractPlugin.extract("style-loader", "css!autoprefixer!less"),
+    //         js: "babel"
+    //     }
+    // },
+    // 配置babel转化成es5的语法
+    // babel: {
+    //     presets: ["es2015", "stage-0"],
+    //     plugins: ["transform-runtime", "transform-object-assign"]
+    // },
+    // resolve: {
+    //     // require时省略的扩展名，如：require("module") 不需要module.js
+    //     extensions: ["", ".js", ".less", ".es6", ".vue", ".html"],
+    //     // 别名，可以直接使用别名来代表设定的路径以及其他
+    //     alias: {
+    //
+    //     }
+    // },
+    // plugins: [
+    //
+    // ]
+};
+
+module.exports = config;
